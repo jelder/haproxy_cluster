4e5ffee75fa6ef5edb491c0103fdfc339914ce65
Jacob Elder <jacob@boundlesslearning.com>
2012-07-23 14:40:30 -0400

    Major landmark passed: ruby -Ilib bin/check_haproxy --eval 'wait_for(true){ api.servers.map{|s|s.ok?} }' http://localhost:{8080,8181}/stats

--- lib/haproxy_cluster.rb
+++ lib/haproxy_cluster.rb
@@ -13,35 +13,53 @@ class HAProxyCluster
     threads.each{|t|t.join}
   end
 
-  def eval(string)
+  # Poll the entire cluster using exponential backoff until the the given block's return value matches the condition (expressed as boolean or range).
+  # 
+  # wait_for(1!=1){false} #=> true
+  # wait_for(1==1){true}  #=> true
+  # wait_for(1..3){2}     #=> true
+  # wait_for(true){sleep} #=> Timeout
+  def wait_for (condition, &code)
+    results = members_exec &code
+    delay = 1.5
+    loop do
+      if check_all condition, results.values.flatten
+        return true
+      end
+      if delay > 60
+        puts "Too many timeouts, giving up"
+        return false
+      end
+      delay *= 2
+      sleep delay
+      results = members_exec &code
+    end
+  end
+
+  def members_exec (&code)
     threads = []
     results = {}
     @members.each do |member|
       threads << Thread.new do
-        results[member.to_s] = Kernel.eval(string, member.get_binding)
+        results[member.to_s] = member.instance_exec &code
       end
     end
     threads.each{|t|t.join}
     return results
   end
 
-  # Poll the entire cluster using exponential backoff until the the given block's return value matches the condition (expressed as boolean or range).
-  # 
-  # wait_for(1!=1){false}
-  # wait_for(1==1){true}
-  # wait_for(1..3){2}
-  def wait_for(condition)
-    raise ArgumentError.new("block required") unless block_given?
-    # eval here
-    case condition.class.to_s.downcase.to_sym
-    when :range
-      puts "range: #{condition}"
-    when :trueclass, :falseclass
-      puts "boolean: #{condition}"
+  private
+ 
+  def check_all (condition, values)
+    case condition.class.to_s
+    when "Range"
+      values.each{ |v| return false unless condition.cover? v }
+    when "TrueClass", "FalseClass" 
+      values.each{ |v| return false unless v == condition }
     else
-       raise ArgumentError.new("TrueClass, FalseClass, or Range expected")
+      raise ArgumentError.new("Got #{condition.class.to_s} but TrueClass, FalseClass, or Range expected")
     end
+    return true
   end
-
 
 end
--- lib/haproxy_instance/cli.rb
+++ lib/haproxy_instance/cli.rb
@@ -3,22 +3,21 @@ require 'optparse'
 require 'pp'
 require 'thread'
 require 'smart_colored/extend'
-require 'haproxy_instance'
 require 'haproxy_cluster'
 
-urls = []
 code_string = ""
 OptionParser.new do |opts|
-  opts.banner = "Inspect and manipulate collections of HAProxy instances"
-  opts.on("-i", "--instance URL", "Instance status page URL (may be specified more than once)") do |o|
-    urls << o
-  end
-  opts.on("-e", "--eval CODE", "Ruby code block to be evaluated on each instance") do |o|
+  opts.banner = "Usage: #{File.basename $0} ARGS URL [URL] [...]"
+  opts.on("-e", "--eval CODE", "Ruby code block to be evaluated") do |o|
     code_string = o
   end
   opts.on("-v", "--verbose", "Verbose logging") do
     RestClient.log = STDERR
   end
+  opts.separator "URL should be the root of an HA Proxy status page, either http:// or https://"
 end.parse!
+urls = ARGV
 
-pp HAProxyCluster.new(urls).eval(code_string)
+cluster = HAProxyCluster.new(urls)
+pp Kernel.eval(code_string, cluster.instance_eval("binding"))
+
