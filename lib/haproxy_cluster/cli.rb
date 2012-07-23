c0c6aa136671ba862b3e6eb0f39dac102e72de62
Jacob Elder <jacob@boundlesslearning.com>
2012-07-23 18:26:33 -0400

    Feature complete, time to publish.

--- lib/haproxy_cluster/cli.rb
+++ lib/haproxy_cluster/cli.rb
@@ -1,30 +1,61 @@
 require 'rubygems'
 require 'optparse'
+require 'ostruct'
 require 'pp'
 require 'thread'
+require 'timeout'
+require 'haproxy_cluster/version'
 require 'haproxy_cluster'
 
-code_string = ""
+options = OpenStruct.new
 OptionParser.new do |opts|
   opts.banner = "Usage: #{File.basename $0} ARGS URL [URL] [...]"
-  opts.on("-e", "--eval CODE", "Ruby code block to be evaluated") do |o|
-    code_string = o
+  opts.on("-e", "--eval=CODE", "Ruby code block to be evaluated") do |o|
+    options.code_string = o
   end
   opts.on("-v", "--verbose", "Verbose logging") do
+    # TODO Need better coverage here
     RestClient.log = STDERR
+  end
+  opts.on("--csv", "Assume result will be an Array of Arrays and emit as CSV") do
+    options.csv = true
+  end
+  opts.on("-t", "--timeout=SECONDS", "Give up after TIMEOUT seconds") do |o|
+    options.timeout = o.to_f
+  end
+  opts.on_tail("--version", "Show version") do
+    puts HAProxyCluster::Version
+    exit
   end
   opts.separator "URL should be the root of an HA Proxy status page, either http:// or https://"
 end.parse!
-urls = ARGV
+options.urls = ARGV
 
-cluster = HAProxyCluster.new(urls)
-result = Kernel.eval(code_string, cluster.instance_eval("binding"))
-case result.class.to_s
-when "TrueClass","FalseClass"
-  exit result.true ? 0 : 1
-when "Hash", "Array"
-  pp result
-else
-  puts result
+
+if options.code_string
+
+  if options.timeout
+    result = Timeout::timeout(options.timeout) do 
+      Kernel.eval( options.code_string, HAProxyCluster.new(options.urls).instance_eval("binding") )
+    end
+  else
+    result = Kernel.eval( options.code_string, HAProxyCluster.new(options.urls).instance_eval("binding") )
+  end
+
+  case result.class.to_s
+  when "TrueClass","FalseClass"
+    exit result == true ? 0 : 1
+  when "Hash"
+    pp result
+  when "Array"
+    if options.csv
+      result.each{|row| puts row.to_csv}
+    else
+      pp result
+    end
+  else
+    puts result
+  end
+
 end
 
